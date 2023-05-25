f2dea236dd35a20f30364b8829e58a061fe92c7d
Vasilevich Dmitry <vasilevich.dmt@gmail.com>
2023-05-25 19:34:25 +0300

    Update cors middleware

--- src/main.py
+++ src/main.py
@@ -12,8 +12,9 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
     allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
+    allow_methods=["GET", "POST", "OPTIONS", "DELETE", "PATCH", "PUT"],
+    allow_headers=["Content-Type", "Set-Cookie", "Access-Control-Allow-Headers", "Access-Control-Allow-Origin",
+                   "Authorization", "Access-Control-Allow-Credentials"],
 )
 
 app.include_router(api_router, prefix=settings.API_V1_STR)
